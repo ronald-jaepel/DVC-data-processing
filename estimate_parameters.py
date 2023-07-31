37f4febfe7f50e9edd23a1db9cd7ea377bef35d0
Jäpel, Ronald <r.jaepel@fz-juelich.de>
2023-07-31 17:02:41 +0200

    Add results file

--- estimate_parameters.py
+++ estimate_parameters.py
@@ -1,13 +1,16 @@
-# %%
 import dvc.api
 import os
-# from dvc.repo import add as dvc_add
 from dvc.repo import Repo as DvcRepo
 import matplotlib.pyplot as plt
 import numpy as np
 from git.repo import Repo as GitRepo
 
+
+def update_package_list():
+    os.system("conda env export > environment.yml")
+
 # %%
+
 results_dir = r"results/parameter_estimation/example_column/column_transport"
 
 if not os.path.exists(results_dir):
@@ -27,8 +30,22 @@ with dvc.api.open(
     ax.plot(data[:, 0], data[:, 1])
     plt.savefig(os.path.join(results_dir, "input.png"))
     dvc_repo.add(results_dir)
+    update_package_list()
     git_repo.git.add(".")
     commit_return = git_repo.git.commit("-m", "Add results file")
     print(commit_return)
 
 # %%
+
+url = r'https://github.com/ronald-jaepel/DVC-data-test'
+out = r"raw_data\non-interacting_tracer\non_pore_penetrating_tracer.csv"
+path = r"data\chromatograms\example_column\non-interacting_tracer\non_pore_penetrating_tracer.csv"
+
+dvc_repo.imp(url=url, path=path, out=out)
+# %%
+
+url = r'https://github.com/ronald-jaepel/DVC-data-test'
+out = r"raw_data"
+path = r"data\chromatograms\example_column\non-interacting_tracer"
+
+dvc_repo.imp(url=url, path=path, out=out)
